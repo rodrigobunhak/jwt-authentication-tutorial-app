6424255ea9d922a7cea52b91369e303ae83facd1
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2024-01-18 16:36:53 -0300

    Add refresh token concept

--- server.js
+++ server.js
@@ -7,20 +7,14 @@ const port = 3000;
 
 app.use(express.json());
 
+let refreshTokens = [];
+
 app.get('/status', (req, res) => {
   res.json({ status: 'API is alive' });
 });
 
 app.get('/content', authenticationToken, (req, res) => {
   res.json({ status: 'Secret content' });
-});
-
-app.get('/login', (req, res) => {
-  // Authentication User
-  const username = req.body.username;
-  const user = { name: username };
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-  res.json({ accessToken: accessToken });
 });
 
 function authenticationToken(req, res, next) {
@@ -33,6 +27,37 @@ function authenticationToken(req, res, next) {
     req.user = user;
     next();
   })
+}
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token;
+  console.log('recebendo refresh token ', refreshToken);
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken });
+  })
+})
+
+app.get('/login', (req, res) => {
+  // Authentication User
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+  res.sendStatus(204);
+});
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
 }
 
 app.listen(port, () => {
